a059bac999e03718159341a87a8d6de53dab093a
Igor Fedonenkov <fedaligo@mail.ru>
2020-08-26 20:26:41 +0300

    Created Angular project. Added test page with info from backend.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Collection} from './entity/collection';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontCollector';
+
+  collection: Collection[];
+  displayedColumns: string[] = ['id', 'picture'];
+
+  constructor(private http: HttpClient) {
+    this.http.get<Collection[]>('http://localhost:5000/task').subscribe(result => {
+      // вызов RESTful сервиса и запись результата в переменную
+      this.collection = result;
+      console.log(result[0]);
+    });
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+
+import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTableModule,
+    CdkTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
